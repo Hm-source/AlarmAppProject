0ad266acb502b6121e12cc637476f55a87c48fd6
Hm-source <swo8852@gmail.com>
2020-07-01 21:23:49 +0900

    현재 시간 출력하는 activity_main.xml완성

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.alarmapp;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 텍스트뷰 받아와서
+        textView = findViewById(R.id.timeview);
     }
-}
+} // MainActivity
