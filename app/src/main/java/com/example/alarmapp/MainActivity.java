f25d3a7294ca3bb06243a1678fc97469a2bd7819
Hm-source <swo8852@gmail.com>
2020-07-01 21:52:59 +0900

    현재 시간 출력하는 코드(thread 사용)

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.alarmapp;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
 
 
 
@@ -16,5 +16,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 텍스트뷰 받아와서
         textView = findViewById(R.id.timeview);
+        // 쓰레드를 이용해서 시계표시
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (!isInterrupted()) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Calendar calendar = Calendar.getInstance(); // 칼렌다 변수
+                            int year = calendar.get(Calendar.YEAR); // 년
+                            int month = calendar.get(Calendar.MONTH); // 월
+                            int day = calendar.get(Calendar.DAY_OF_MONTH); // 일
+                            int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
+                            int minute = calendar.get(Calendar.MINUTE); // 분
+                            int second = calendar.get(Calendar.SECOND); // 초
+
+                            textView.setText( year + "년 " + month + "월 " + day + "일\n" + hour + "시 " + minute + "분" + second + "초 \n");
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000); // 1000 ms = 1초
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } // while
+            } // run()
+        }; // new Thread() { };
+
+        thread.start();
     }
 } // MainActivity
