8529de22579b404c694ad833bb12e8786caaa9da
Hm-source <swo8852@gmail.com>
2020-07-02 23:41:06 +0900

    현재 실시간 계속 보이게 다시 커밋.

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -37,36 +37,36 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnCalendar).setOnClickListener(mClickListener);
         findViewById(R.id.btnAlarm).setOnClickListener(mClickListener);
 
-//        // 텍스트뷰 받아와서
-//        textView = findViewById(R.id.timeview);
-//        // 쓰레드를 이용해서 시계표시
-//        Thread thread = new Thread() {
-//            @Override
-//            public void run() {
-//                while (!isInterrupted()) {
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            Calendar calendar = Calendar.getInstance(); // 칼렌다 변수
-//                            int year = calendar.get(Calendar.YEAR); // 년
-//                            int month = calendar.get(Calendar.MONTH); // 월
-//                            int day = calendar.get(Calendar.DAY_OF_MONTH); // 일
-//                            int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
-//                            int minute = calendar.get(Calendar.MINUTE); // 분
-//                            int second = calendar.get(Calendar.SECOND); // 초
-//
-//                            textView.setText(year + "년 " + (month + 1) + "월 " + day + "일\n" + hour + "시 " + minute + "분" + second + "초 \n");
-//                        }
-//                    });
-//                    try {
-//                        Thread.sleep(1000); // 1000 ms = 1초
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                } // while
-//            } // run()
-//        }; // new Thread() { };
-//        thread.start();
+        // 텍스트뷰 받아와서
+        textView = findViewById(R.id.timeview);
+        // 쓰레드를 이용해서 시계표시
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (!isInterrupted()) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Calendar calendar = Calendar.getInstance(); // 칼렌다 변수
+                            int year = calendar.get(Calendar.YEAR); // 년
+                            int month = calendar.get(Calendar.MONTH); // 월
+                            int day = calendar.get(Calendar.DAY_OF_MONTH); // 일
+                            int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
+                            int minute = calendar.get(Calendar.MINUTE); // 분
+                            int second = calendar.get(Calendar.SECOND); // 초
+
+                            textView.setText(year + "년 " + (month + 1) + "월 " + day + "일\n" + hour + "시 " + minute + "분" + second + "초 \n");
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000); // 1000 ms = 1초
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } // while
+            } // run()
+        }; // new Thread() { };
+        thread.start();
 
     } //onCreate
     /* 날짜 표시 */
