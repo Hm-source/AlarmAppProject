e4194c0d7d3756ef148a0171c0b3391cda1ba2ca
Hm-source <swo8852@gmail.com>
2020-07-02 22:23:18 +0900

    displayDate()

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -1,33 +1,34 @@
 package com.example.alarmapp;
 
-import android.app.AlarmManager;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    final TimePicker picker = (TimePicker) findViewById(R.id.timePicker);
-    private AlarmManager alarmManager;
-    private int hour, minute;
+    private Calendar calendar;
+    private TimePicker timePicker;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.calendar = Calendar.getInstance();
+        // 현재 날짜 표시
+        displayDate();
+        this.timePicker = findViewById(R.id.timePicker);
+
+        findViewById(R.id.btnCalendar).setOnClickListener(mClickListener);
+        findViewById(R.id.btnAlarm).setOnClickListener(mClickListener);
+
         // 텍스트뷰 받아와서
         textView = findViewById(R.id.timeview);
         // 쓰레드를 이용해서 시계표시
@@ -59,77 +60,13 @@ public class MainActivity extends AppCompatActivity {
         }; // new Thread() { };
         thread.start();
 
-
-        picker.setIs24HourView(true);
-        //미리 설정한 시간이 있으면 그 값으로 보인다.
-        SharedPreferences sharedPreferences = getSharedPreferences("daily alarm", MODE_PRIVATE);
-        long millis = sharedPreferences.getLong("nextNotifyTime", Calendar.getInstance().getTimeInMillis());
-        //없으면 기본 값으로 현재 시간을 보여준다.
-        Calendar nextNotifyTime = new GregorianCalendar();
-
-        nextNotifyTime.setTimeInMillis(millis);
-        Date nextDate = nextNotifyTime.getTime();
-        String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(nextDate);
-        Toast.makeText(getApplicationContext(), "[처음 실행시] 다음 알람은 " + date_text + "으로 설정되었습니다!!", Toast.LENGTH_SHORT).show();
-
-        // 이전에 설정해둔 값으로 TimePicker를 초기화 합니다.
-        Date currentTime = nextNotifyTime.getTime();
-        SimpleDateFormat HourFormat = new SimpleDateFormat("kk", Locale.getDefault());
-        SimpleDateFormat MinuteFormat = new SimpleDateFormat("mm", Locale.getDefault());
-
-        int pre_hour = Integer.parseInt(HourFormat.format(currentTime));
-        int pre_minute = Integer.parseInt(MinuteFormat.format(currentTime));
-
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            picker.setHour(pre_hour);
-            picker.setMinute(pre_minute);
-        } else {
-            picker.setCurrentHour(pre_hour);
-            picker.setCurrentMinute(pre_minute);
-        }
-
-        Button button = (Button) findViewById(R.id.btn_1);
-        //버튼 클릭 시 동작
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-
-                int hour, hour_24, minute;
-                String am_pm;
-                if (Build.VERSION.SDK_INT >= 23) {
-                    hour_24 = picker.getHour();
-                    minute = picker.getMinute();
-                } else {
-                    hour_24 = picker.getCurrentHour();
-                    minute = picker.getCurrentMinute();
-                }
-                if (hour_24 > 12) {
-                    am_pm = "PM";
-                    hour = hour_24 - 12;
-                } else {
-                    hour = hour_24;
-                    am_pm = "AM";
-                }
-                // 현재 지정된 시간으로 알람 시간 설정
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.set(Calendar.HOUR_OF_DAY, hour_24);
-                calendar.set(Calendar.MINUTE, minute);
-                calendar.set(Calendar.SECOND, 0);
-
-                // 이미 지난 시간을 지정했다면 다음날 같은 시간으로 설정
-                if (calendar.before(Calendar.getInstance())) {
-                    calendar.add(Calendar.DATE, 1);
-                }
-
-                Date currentDateTime = calendar.getTime();
-                String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
-                Toast.makeText(getApplicationContext(), date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
-            }
-
-        });
     } //onCreate
+    /* 날짜 표시 */
+    private void displayDate() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        ((TextView) findViewById(R.id.txtDate)).setText(format.format(this.calendar.getTime()));
+    }
+
 
 
 }// MainActivity
